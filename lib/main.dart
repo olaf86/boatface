1b520359c0c43238b2ef9518fffc0c595004a0db
olaf86 <yogawa0417@gmail.com>
2026-03-20 15:34:16 +0900

    Guard against default Flutter entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
-import 'main_stg.dart' as stg;
-
-Future<void> main() => stg.main();
+Never main() {
+  throw UnsupportedError(
+    'Use an environment-specific entrypoint: '
+    '`flutter run --flavor stg -t lib/main_stg.dart` or '
+    '`flutter run --flavor prod -t lib/main_prod.dart`.',
+  );
+}
