c205b8e73b40dcab5fc81ff208a63ef3e6f9183d
olaf86 <yogawa0417@gmail.com>
2026-03-22 14:18:08 +0900

    Fix ranking provider type annotation

--- lib/features/ranking/application/ranking_providers.dart
+++ lib/features/ranking/application/ranking_providers.dart
@@ -3,8 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../data/ranking_repository.dart';
 import '../domain/ranking_models.dart';
 
-final AutoDisposeFutureProviderFamily<RankingSnapshot, RankingRequest>
-rankingSnapshotProvider = FutureProvider.autoDispose
+final rankingSnapshotProvider = FutureProvider.autoDispose
     .family<RankingSnapshot, RankingRequest>((Ref ref, RankingRequest request) {
       return ref
           .watch(rankingRepositoryProvider)
